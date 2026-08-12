edec09b44bb1395990cbd430d9265daf09065cc2
sigeinbr <eliel.ferreira@sigein.com.br>
2026-08-12 20:41:45 -0300

    atualização do enum_tipo_token

---
+++ Scripts/V202608121700__adicionar_convite_usuario_enum_tipo_token.sql
@@ -0,0 +1,3 @@
+alter type adm.enum_tipo_token add value 'convite_usuario';
+alter type adm.enum_tipo_token add value 'criacao_conta';
+alter type adm.enum_tipo_token add value 'alteracao_email_conta';
